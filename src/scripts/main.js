8d495b5550c3bc745f3cc0489d1face54cb95ff0
Serhii-Khobotov <hobbitura@gmail.com>
2024-11-21 15:52:49 +0200

    My solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+    resolve(`First promise was resolved`);
+    setTimeout(() => {
+        reject(new Error(`First promise was rejected`))
+    },3000)
+})
+
+firstPromise.then(data => console.log(data))
