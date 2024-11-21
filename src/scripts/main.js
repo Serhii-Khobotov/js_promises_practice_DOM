e34ce43f1e0f2f0658c2521adf03be1b7d1413ea
Serhii-Khobotov <hobbitura@gmail.com>
2024-11-21 16:04:14 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,82 @@
 'use strict';
 
-const firstPromise = new Promise((resolve, reject) => {
-    resolve(`First promise was resolved`);
-    setTimeout(() => {
-        reject(new Error(`First promise was rejected`))
-    },3000)
-})
+let leftClicked = false;
+let rightClicked = false;
 
-firstPromise.then(data => console.log(data))
+function createMessage(message, className) {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.classList.add(className);
+  div.textContent = message;
+  document.body.append(div);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error(`First promise was rejected`));
+  }, 3000);
+
+  document.addEventListener(
+    'click',
+    () => {
+      leftClicked = true;
+
+      resolve('First promise was resolved');
+    },
+    { once: true },
+  );
+});
+
+firstPromise
+  .then((data) => {
+    createMessage(data, 'success');
+  })
+  .catch((error) => {
+    createMessage(error, 'error');
+  });
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener(
+    'click',
+    () => {
+      leftClicked = true;
+
+      resolve('Second promise was resolved');
+    },
+    { once: true },
+  );
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    rightClicked = true;
+
+    resolve('Second promise was resolved');
+  });
+});
+
+secondPromise.then((data) => {
+  createMessage(data, 'success');
+});
+
+const thirdPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    leftClicked = true;
+
+    if (leftClicked && rightClicked) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  document.addEventListener('contextmenu', () => {
+    rightClicked = true;
+
+    if (leftClicked && rightClicked) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+thirdPromise.then((data) => {
+  createMessage(data, 'success');
+});
